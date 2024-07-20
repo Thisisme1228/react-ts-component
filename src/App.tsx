c9f4b1d8f2a8272ea47bc9ec7007ad52b6d4cad0
Helene <878723827@qq.com>
2024-07-21 04:08:58 +0800

    release components to npm

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,17 @@
-import React from 'react';
-import Button from "./components/Button";
-import Menu from "./components/Menu";
-import MenuItem from "./components/Menu/menuItem";
-import SubMenu from "./components/Menu/subMenu";
-import Tabs from "./components/Tabs";
-import {TabsItemsProps} from "./components/Tabs/TabItem";
-import {faChevronDown} from '@fortawesome/free-solid-svg-icons';
-import Icon from './components/Icon/Icon';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
-    const items: TabsItemsProps[] = [
-        {
-            label: <>tab1 <Icon icon={faChevronDown} className='icon-dark' size="1x"/></>,
-            children: <ul>
-                <ol>1</ol>
-                <ol>1</ol>
-                <ol>1</ol>
-                <ol>1</ol>
-                <ol>1</ol>
-                <ol>1</ol>
-                <ol>1</ol>
-                <ol>1</ol>
-                <ol>1</ol>
-                <ol>1</ol>
-            </ul>,
-        },
-        {
-            label: 'tab2',
-            children: 'this is tab2',
-            disabled: true,
-        },
-        {
-            label: 'tab3',
-            children: 'this is tab3',
-        }
-    ]
+    const [title, setTitle] = useState('');
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts/1',{headers:{}}).then(res => {
+            console.log(res);
+            setTitle(res.data.title);
+        })
+    });
     return (
         <div className="App">
-            <header className="App-header">
-                <h1>Hello World</h1>
-                <h2>Hello World</h2>
-                <h3>Hello World</h3>
-                <h4>Hello World</h4>
-                <h5>Hello World</h5>
-                <h6>Hello World</h6>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <hr></hr>
-                <code>
-                    const a = 'b'
-                </code>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <hr/>
-                <Button disabled size='lg' className='custom'>Hello</Button>
-                <Button btnType='danger' size='sm'>111</Button>
-                <Button btnType='default' size='lg'>Hello</Button>
-                <Button btnType='primary'>Hello</Button>
-                <Button btnType='link' href='http://www.baidu.com' size='sm' onClick={(e) => {
-                    e.preventDefault();
-                    alert(1)
-                }}>Hello</Button>
-                <hr/>
-                <Menu defaultIndex='2' mode='vertical'>
-                    <MenuItem>
-                        one
-                    </MenuItem>
-                    <SubMenu title='dropdown'>
-                        <MenuItem>123</MenuItem>
-                        <MenuItem>234</MenuItem>
-                    </SubMenu>
-                    <MenuItem>
-                        two
-                    </MenuItem>
-                    <MenuItem disabled={true}>
-                        three
-                    </MenuItem>
-                </Menu>
-                <Menu mode='horizontal'>
-                    <MenuItem disabled={true}>
-                        one
-                    </MenuItem>
-                    <SubMenu title='dropdown'>
-                        <MenuItem>123</MenuItem>
-                        <MenuItem>234</MenuItem>
-                    </SubMenu>
-                    <MenuItem>
-                        two
-                    </MenuItem>
-                    <MenuItem>
-                        three
-                    </MenuItem>
-                </Menu>
-                <Tabs items={items} defaultActiveKey='2' mode='card' onSelect={(index) => {
-                    alert(index)
-                }}/>
-            </header>
+            {title}
         </div>
     );
 }
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,3 +1,17 @@
-import Menu from "./menu";
+import {FC} from 'react'
 
-export default Menu;
+import Menu, {MenuProps} from "./menu";
+import SubMenu, {SubMenuProps} from "./subMenu";
+import MenuItem, {MenuItemProps} from "./menuItem";
+
+export type IMenuComponent = FC<MenuProps> & {
+    Item: FC<MenuItemProps>,
+    SubMenu: FC<SubMenuProps>
+}
+
+const TransMenu = Menu as IMenuComponent;
+
+TransMenu.Item = MenuItem
+TransMenu.SubMenu = SubMenu
+
+export default TransMenu;
